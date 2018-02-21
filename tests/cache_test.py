8c85b0d7a5d765627d156bbd5564576f9ba48480
Alex Casalboni <alexsaxmib@gmail.com>
2018-02-21 11:46:11 +0100

    Moved files to proper folders (ssm_cache module)

--- tests/cache_test.py
+++ tests/cache_test.py
@@ -1,8 +1,12 @@
+import os
+import sys
 import unittest
 from datetime import datetime, timedelta
-from cache import SSMParameter, InvalidParam
 from moto import mock_ssm
 import boto3
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from ssm_cache import SSMParameter, InvalidParam
 
 
 @mock_ssm
