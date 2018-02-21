5e01ebdd5decfc1581a32fb92de227fb89d40891
Alex Casalboni <alexsaxmib@gmail.com>
2018-02-21 11:47:02 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README", 'r') as f:
+    long_description = f.read()
+
 setup(
-    name='ssm-cache',
-    version='1.0',
+    name='ssm_cache',
+    version='0.1',
     description='AWS System Manager Parameter Store caching client for Python',
+    long_description=long_description,
+    license="MIT",
     author='Alex Casalboni',
     author_email='alex@alexcasalboni.com',
     url='https://github.com/alexcasalboni/ssm-cache-python',
