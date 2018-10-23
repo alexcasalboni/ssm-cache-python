3a33f9d1077a60814642d2fc6d99b794fa68c077
Alex Casalboni <alexsaxmib@gmail.com>
2018-10-23 22:00:20 +0000

    minor version bump (2.6)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 setup(
     name='ssm-cache',
-    version='2.5',
+    version='2.6',
     description='AWS System Manager Parameter Store caching client for Python',
     long_description=LONG_DESCRIPTION,
     keywords=['aws', 'amazon-web-services', 'aws-lambda', 'aws-ssm', 'parameter-store'],
@@ -17,7 +17,7 @@ setup(
     author='Alex Casalboni',
     author_email='alex@alexcasalboni.com',
     url='https://github.com/alexcasalboni/ssm-cache-python',
-    download_url='https://github.com/alexcasalboni/ssm-cache-python/archive/2.5.tar.gz',
+    download_url='https://github.com/alexcasalboni/ssm-cache-python/archive/2.6.tar.gz',
     packages=find_packages(),
     install_requires=['boto3', 'future'],
 )
